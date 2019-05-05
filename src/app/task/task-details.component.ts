de00dab454f9f828d2fb33ac4c58fc99ba4a913c
Piotr Zysk <pizy123@gmail.com>
2019-05-05 09:46:15 +0200

    Added handling input parameters by TaskDetailsComponent.

--- src/app/task/task-details.component.ts
+++ src/app/task/task-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { TaskService } from './task.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ITask } from './task';
 
 @Component({
   selector: 'app-task-details',
@@ -7,9 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TaskDetailsComponent implements OnInit {
 
-  constructor() { }
+  pageTitle: string = 'Task Details';
+  task: ITask;
+
+  constructor(private taskService: TaskService, private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    let id = +this.route.snapshot.paramMap.get('id');
+
+    this.task = {
+      'id': id,
+      'name': 'test',
+      'description': 'test decription',
+      'done': false
+    }
+
   }
 
 }
--- src/app/task/task.ts
+++ src/app/task/task.ts
@@ -1,6 +1,6 @@
 export interface ITask {
     id: number;
     name: string;
-    decription: string;
+    description: string;
     done: boolean;
 }
